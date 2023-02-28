e0a984f16b2ed89d07dada5e86bf1d7b9b369815
Oliver Fields <oliver@phnd.net>
2023-02-28 21:42:24 +0100

    commercial baby

--- commercial/direct.sh
+++ commercial/direct.sh
@@ -29,7 +29,6 @@ cd screenshots || exit 1
 
 # Chop off title bar and scroll bar
 for i in *.gif; do
-  echo $i
   convert $i -gravity East -chop 10x0 ../frames/$i 
   convert ../frames/$i -chop 0x25 ../frames/$i
 done
@@ -40,9 +39,27 @@ annotate "01_create-standup-command.gif" "👆️ Create a new note named standu
 annotate "02_create-standup-note.gif" "👆️ Note opens in vi, add a new task\nwith status TODO\n\nTasks are any line containing a valid Status\n\nAdd hashtags anywhere to create tags\n\nNotes are plain text files" "+40+40"
 annotate "03_list-tasks-command.gif" "👆️ List open tasks from all notes files" "+40+40"
 annotate "04_list-tasks-before-update.gif" "👈️ Cindy task is done💪\nType 1 + Enter to select\ntask and open note in vi" "+440+70"
-annotate "05_update-task-call-cindy-1.gif" "Vi opend on line 15👌\nNow change TODO to DONE\nto close task" "+270+300"
+annotate "05_update-task-call-cindy-1.gif" "Vi opened on line 15👌\nNow change TODO to DONE\nto close task" "+270+300"
 annotate "06_update-task-call-cindy-2.gif" "✅👍️" "+360+310"
 annotate "07_note-markup.gif" "Notes are just plain text files.\nDun should play nice with your\npreffered note taking markup👌" "+300+80"
-annotate "08_list-tasks-after-update.gif" "Return to updated\ntask list💫" "+450+110"
+annotate "08_list-tasks-after-update.gif" "Closing vi returns to\nupdated task list💫" "+450+110"
 annotate "09_list-projectx-done-tasks-command.gif" "👆️ List all DONE tasks tagged #ProjectX\n\nTab completion for\n- Tags\n- Statuses\n- Arguments" "+40+40"
 #annotate "10_list-projectx-done-task-list.gif" "DONE🤩" "+520+40"
+
+cd ..
+gifsicle --loopcount=forever --colors 256 \
+-d 250 title.gif \
+-d 500 \
+frames/01_create-standup-command.gif \
+-d 1500 \
+frames/02_create-standup-note.gif \
+frames/03_list-tasks-command.gif \
+frames/04_list-tasks-before-update.gif \
+frames/05_update-task-call-cindy-1.gif \
+frames/06_update-task-call-cindy-2.gif \
+frames/07_note-markup.gif \
+frames/08_list-tasks-after-update.gif \
+frames/09_list-projectx-done-tasks-command.gif \
+frames/10_list-projectx-done-task-list.gif \
+> dun-commercial.gif 
+
